d2483c7764acc091af9ce027379a83cfce54f0a4
lgalban <83791061+lgalban@users.noreply.github.com>
2021-06-23 11:53:37 -0300

    Add files via upload

--- TP1/Ejercicio2/main.cpp
+++ TP1/Ejercicio2/main.cpp
@@ -2,13 +2,23 @@
 
 using namespace std;
 
-int main1()
+int main()
 {
     int x, y, ma, mb, ba, bb;
     cout<< "Dadas la funcion y = m1*X + b1 y la funcion y = m2*X + b2, ingrese en orden m1, b1, m2 y b2"<< endl;
     cin>> ma >> ba >> mb >> bb;
-    x=(bb-ba)/(ma-mb);
-    y=ma*x+ba;
-    cout<< "Las rectas intersectan en el punto ("<< x << ";" << y << ")" << endl;
+    if (ma==mb && ba<>bb)
+        {cout<< "las rectas son paralelas por lo tanto no se intersectan"<< endl;}
+    else {
+        if (ma==mb && ba==bb)
+        {
+            cout<< "las rectas son las mismas por lo que son compatibles indetermindos"
+        }
+        else
+            {
+                x=(bb-ba)/(ma-mb);
+                y=ma*x+ba;
+                cout<< "Las rectas intersectan en el punto ("<< x << ";" << y << ")" << endl;
+            }
     return 0;
 }
